7404c8b1a11cd058d00a62177e47bfa299564e27
André Meister <am@mega.co.nz>
2014-12-04 11:29:00 +1300

    PCR add functionality tentatively done

--- src/megaclient.cpp
+++ src/megaclient.cpp
@@ -3159,6 +3159,8 @@ void MegaClient::sc_opc()
                 if (dts != 0)
                 {
                     // this is a delete, find the existing object in state
+                    pcr->uts = dts;
+                    pcr->changed.deleted = true;
                 }
                 else if (pcr)
                 {
@@ -4214,6 +4216,7 @@ void MegaClient::readipc(JSON *j)
         {
             m_time_t ts = 0;
             m_time_t uts = 0;
+            int ps = 0;
             const char *m = NULL;
             const char *msg = NULL;
             handle p = UNDEF;
@@ -4223,6 +4226,7 @@ void MegaClient::readipc(JSON *j)
             {
                 switch (j->getnameid()) {
                     case MAKENAMEID2('p', 's'):
+                        ps = j->getint();
                         break;
                     case 'm':
                         m = j->getvalue();
@@ -4237,7 +4241,7 @@ void MegaClient::readipc(JSON *j)
                         msg = j->getvalue();
                         break;
                     case 'p':
-                        p = j->gethandle();
+                        p = j->gethandle(MegaClient::PCRHANDLE);
                         break;
                     case EOO:
                         done = true;
