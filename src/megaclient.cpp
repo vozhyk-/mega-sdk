88de7ac23c2ab60d3619251f8f9d8c14a489071d
Victor Teniente <vtmateos@gmail.com>
2015-04-10 14:05:47 +0200

    Fixes for contact features

--- src/megaclient.cpp
+++ src/megaclient.cpp
@@ -3262,9 +3262,13 @@ void MegaClient::sc_ipc()
 
                 if (dts != 0)
                 {
-                    // this is a delete, find the existing object in state
-                    pcr->uts = dts;
-                    pcr->changed.deleted = true;
+                    //Trying to remove an ignored request
+                    if(pcr)
+                    {
+                        // this is a delete, find the existing object in state
+                        pcr->uts = dts;
+                        pcr->changed.deleted = true;
+                    }
                 }
                 else if (pcr && rts != 0)
                 {
@@ -5621,7 +5625,7 @@ void MegaClient::notifyuser(User* u)
 // queue pcr for notification
 void MegaClient::notifypcr(PendingContactRequest* pcr)
 {
-    if (!pcr->notified)
+    if (pcr && !pcr->notified)
     {
         pcr->notified = true;
         pcrnotify.push_back(pcr);
